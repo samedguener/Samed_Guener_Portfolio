b820e1a83af8220a37acda8e72bedf65ffc0b10f
Samed Güner <doktorgibsonyt@gmail.com>
2018-11-10 17:44:40 +0100

    Add Docker installation
    - remove tag-based build legacy code

--- CI/deploy.sh
+++ CI/deploy.sh
@@ -2,15 +2,38 @@
 PROJECT_ID=$1
 REPO_NAME=$2
 BRANCH_NAME=$3
-SHORT_SHA=$5
-CLOUDSDK_COMPUTE_ZONE=$6
-CLOUDSDK_CONTAINER_CLUSTER=$7
+# using SHORT_SHA as tag, since tags are currently not supported by Github GCB integration
+TAG_NAME=$4
+CLOUDSDK_COMPUTE_ZONE=$5
+CLOUDSDK_CONTAINER_CLUSTER=$6
 
 if [ $BRANCH_NAME != "master" ]
 then
     echo "We are not in the master branch! Skipping deployment!"
     exit 0
 fi
+
+echo "Updating repositories .."
+apt-get update -y
+echo "Updating repositories .. finished!"
+
+echo "Installing Docker requirements .."
+apt-get install apt-transport-https ca-certificates curl software-properties-common
+echo "Installing Docker requirements .. finished!"
+
+echo "Installing Docker .."
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get install docker-ce
+
+while (! docker stats --no-stream ); do
+  # Docker takes a few seconds to initialize
+  echo "Waiting for Docker to launch..."
+  sleep 1
+done
+
+echo "Installing Docker .. finished!"
 
 echo "Building image $REPO_NAME:$TAG_NAME for $REPO_NAME!"
 
