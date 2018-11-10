97edba150afec9c59a8a493d0ac39429bb1212ad
Samed Güner <doktorgibsonyt@gmail.com>
2018-11-10 17:48:49 +0100

    Remove `sudo` from commands

--- CI/deploy.sh
+++ CI/deploy.sh
@@ -22,7 +22,7 @@ apt-get install -y apt-transport-https ca-certificates curl software-properties-
 echo "Installing Docker requirements .. finished!"
 
 echo "Installing Docker .."
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update -y
 apt-get install -y docker-ce
